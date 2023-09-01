f492df0a8a32482e992fe42caa2b3812d4fb147a
Spiderpat12 <spider.pat.omar@gmail.com>
2023-09-01 09:36:40 -0700

    player 2

--- project_whith_rayan/scripts/Player.gd
+++ project_whith_rayan/scripts/Player.gd
@@ -8,7 +8,14 @@ export var acc : float = 5
 
 var x : float
 var motion = Vector2()
+var chapters : int = 1
+var max_jump = 2
+var jump_count = 0
 
+
+func _ready():
+	chapters = 1
+	pass
 
 
 func _process(delta) -> void:
@@ -21,10 +28,8 @@ func _physics_process(delta) -> void:
 	Movement(delta)
 	Gravity()
 	Jump()
-	
-
-
-
+	chapters()
+	doubleJump()
 
 
 func Movement(delta) -> void:
@@ -35,16 +40,38 @@ func Movement(delta) -> void:
 		motion.x = lerp(motion.x, 0, 5 * delta)
 
 
-
 func Gravity() -> void:
 	if !is_on_floor():
 		motion.y += gravity
 	pass
 
 
-
 func Jump() -> void:
-	pass
+	if is_on_floor() && Input.is_action_just_pressed("ui_up") && chapters != 2:
+		motion.y = -jump
+
+func doubleJump() -> void:
+	if jump_count < max_jump && chapters == 2 or 3:
+		if Input.is_action_just_pressed("ui_up"):
+			motion.y = -jump
+			jump_count += 1
+	if is_on_floor() && jump_count != 0:
+		jump_count = 0
+
+
+
+func chapters() -> void:
+	match chapters:
+		1:
+			#roll_func
+			pass
+		2:
+			#roll_func
+			pass
+		3:
+			pass
+
+
 
 
 
